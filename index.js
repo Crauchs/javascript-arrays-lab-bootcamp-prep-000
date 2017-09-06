3487f6935161627207249bf9232f141ee8be7d5d
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-06 19:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function destructivelyRemoveLastKitten() {kittens.pop(); return kittens}
 function destructivelyRemoveFirstKitten() {kittens.shift(); return kittens}
 function appendKitten(name) {const moreKittens=[kittens, ...name]; return moreKittens}
 function prependKitten(name) {return [...name, kittens]}
-function removeLastKitten() {return [0, kittens.length-1]}
+function removeLastKitten() {return (0, kittens.length-1)}
 function removeFirstKitten() {return kittens.slice(1)}
