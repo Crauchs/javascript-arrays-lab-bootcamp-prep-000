ac515179766ddc74b80cc936026ea82a8a9ab41f
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-06 19:02:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens=["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {return}
+function destructivelyPrependKitten(name) {return}
+function destructivelyRemoveLastKitten() {return}
+function destructivelyRemoveFirstKitten() {return}
+function appendKitten(name) {return}
+function prependKitten(name) {return}
+function removeLastKitten() {return}
+function removeFirstKitten() {return}
